083d780049bde617cd5777b93d7d52a7964e1017
吴瑞祥 <wrx.01@126.com>
2016-11-08 17:10:27 +0800

    添加应该是不会改了.能用就行

---
+++ keepratio.js
@@ -0,0 +1,33 @@
+﻿( function ( $ ) {
+    // Collection method.
+    $.fn.keepratio = function ( config, resize ) {//config中需要最小宽度与高宽比
+        config.minheight = config.minwidth * config.aspectratio;
+
+        setInterval( function () {
+            if ( window.keepratioResizeState != "ok" ) {
+                var height = window.innerHeight;
+                var width = window.innerWidth;
+                if ( width < config.minwidth || height < config.minheight ) {
+                    width = config.minwidth;
+                    height = config.minheight;
+                }
+                else {
+                    if ( height / config.aspectratio > width ) {//判断以哪个为基准
+                        height = width * config.aspectratio;
+                    } else {
+                        width = height / config.aspectratio;
+                    }
+                }
+                $( ".body" ).css( "width", width ).css( "height", height ).css( "margin-top", ( window.innerHeight - height ) > 0 ? (( window.innerHeight - height ) / 2) : 0 );
+                if ( resize ) {
+                    resize();
+                }
+                window.keepratioResizeState = "ok";
+            }
+        }, 500 );
+        $( window ).resize( function () {
+            window.keepratioResizeState = "no";
+        } );
+    };
+
+}( jQuery ) );
